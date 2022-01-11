12c302625b0c403785c7c31229918a170b590910
Matheos Romain <romain.matheos.31@gmail.com>
2022-01-11 19:11:29 +0100

    Script qui converti une heure au format 24 en format 12

---
+++ terre12.js
@@ -0,0 +1,22 @@
+#! node
+// Script qui converti une heure en format 24 en format 12
+
+if (process.argv[2] == undefined) {
+  console.log("Veuillez indiquer au script, un heure au format 24. Exemple : node terre12.js 24:00")
+  return
+}
+
+const heure = process.argv[2].split(":")
+const min = heure[1]
+let hour
+
+if (parseInt(heure[0]) > 12) {
+  hour = heure[0] - 12
+  console.log(hour + ':' + min + 'PM')
+} else if (parseInt(heure[0]) === 00) {
+  hour = 12
+  console.log(hour + ":" + min + 'PM')
+} else {
+  hour = heure[0]
+  console.log(hour + ':' + min + 'AM')
+}
