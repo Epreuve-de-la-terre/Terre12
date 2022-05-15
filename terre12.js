b2506a1b07c5573e774b18ae809a0779afaee68a
RoMTHoS <romain.matheos.31@gmail.com>
2022-05-15 10:54:39 +0200

    Script qui converti une heure au format 24 au format 12

--- terre12.js
+++ terre12.js
@@ -3,7 +3,7 @@
 
 if (process.argv[2] == undefined) {
   console.log(
-    "Veuillez indiquer au script, un heure au format 24. Exemple : node terre12.js 24:00"
+    "Veuillez indiquer au script, un heure au format 24. Exemple : node terre12.js 18:00"
   );
   return;
 }
@@ -11,8 +11,6 @@ if (process.argv[2] == undefined) {
 const heure = process.argv[2].split(":");
 const min = heure[1];
 let hour;
-
-console.log(parseInt(heure[0]));
 
 if (parseInt(heure[0]) > 12) {
   hour = heure[0] - 12;
